0bd4c01d0435c4d4e683d9cf12f936d6b8c25a24
wsx <wushexu@gmail.com>
2020-11-26 19:26:01 +0800

    extract app-material.module

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import { MatMenuModule } from '@angular/material/menu';
+
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatMenuModule
+  ]
+})
+export class AppMaterialModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import {FlexLayoutModule} from '@angular/flex-layout';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import {LayoutModule} from '@angular/cdk/layout';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
-import { MatMenuModule } from '@angular/material/menu';
-import { HttpClientModule } from '@angular/common/http';
+import {AppMaterialModule} from './app-material.module';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -22,9 +14,9 @@ import {NavComponent} from './nav/nav.component';
 import {WeekScheduleComponent} from './week-schedule/week-schedule.component';
 import {MediaQueryStatusComponent} from './common/media/media-query-status.component';
 import {FhComponent} from './fh/fh.component';
-import { MenuComponent } from './menu/menu.component';
-import { StyleManagerService } from './service/style-manager.service';
-import { ThemeService } from './service/theme.service';
+import {MenuComponent} from './menu/menu.component';
+import {StyleManagerService} from './service/style-manager.service';
+import {ThemeService} from './service/theme.service';
 
 @NgModule({
   declarations: [
@@ -40,17 +32,9 @@ import { ThemeService } from './service/theme.service';
     FlexLayoutModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatMenuModule,
-    HttpClientModule
+    AppMaterialModule
   ],
   providers: [StyleManagerService, ThemeService],
   bootstrap: [AppComponent]
