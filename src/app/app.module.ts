37221eda69562e8eee337622e8bb6e406de54164
wsx <wushexu@gmail.com>
2020-11-26 12:43:30 +0800

    flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { WeekScheduleComponent } from './week-schedule/week-schedule.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavComponent} from './nav/nav.component';
+import {WeekScheduleComponent} from './week-schedule/week-schedule.component';
+import { MediaQueryStatusComponent } from './common/media/media-query-status.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    WeekScheduleComponent
+    WeekScheduleComponent,
+    MediaQueryStatusComponent
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
@@ -39,4 +45,5 @@ import { MatSortModule } from '@angular/material/sort';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/common/media/media-query-status.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {MediaChange, MediaObserver} from '@angular/flex-layout';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-media-query-status',
+  templateUrl: './media-query-status.component.html',
+  styleUrls: ['./media-query-status.component.css']
+})
+export class MediaQueryStatusComponent {
+  media$: Observable<MediaChange[]>;
+
+  constructor(media: MediaObserver) {
+    this.media$ = media.asObservable();
+  }
+}
