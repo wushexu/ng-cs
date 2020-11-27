230afd975fc1cfc9bb8be80481c66e1ad085e83e
wsx <wushexu@gmail.com>
2020-11-27 17:06:30 +0800

    add ng2-semantic-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {SuiModule} from 'ng2-semantic-ui';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FhComponent} from './fh/fh.component';
 import {ScheduleService} from './service/schedule.service';
-import { LessonScheduleComponent } from './schedule/lesson-schedule/lesson-schedule.component';
-import { DayScheduleComponent } from './schedule/day-schedule/day-schedule.component';
-import { WeekScheduleComponent } from './schedule/week-schedule/week-schedule.component';
-import { MonthScheduleComponent } from './schedule/month-schedule/month-schedule.component';
-import { TermScheduleComponent } from './schedule/term-schedule/term-schedule.component';
+import {LessonScheduleComponent} from './schedule/lesson-schedule/lesson-schedule.component';
+import {DayScheduleComponent} from './schedule/day-schedule/day-schedule.component';
+import {WeekScheduleComponent} from './schedule/week-schedule/week-schedule.component';
+import {MonthScheduleComponent} from './schedule/month-schedule/month-schedule.component';
+import {TermScheduleComponent} from './schedule/term-schedule/term-schedule.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +31,10 @@ import { TermScheduleComponent } from './schedule/term-schedule/term-schedule.co
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    SuiModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ScheduleService],
   bootstrap: [AppComponent]
